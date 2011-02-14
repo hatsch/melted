f5d22a893d1831476563f9390ff4eaa99a16b010
hatsch <hatsch@servus.at>
2011-02-14 20:32:41 +0100

    first commit

--- js/melted.js
+++ js/melted.js
@@ -1,7 +1,33 @@
+/*
 function refresh() {
- jQuery('#melted_status').load('/melted/refresh #melted_status');
+//   jQuery('#melted_status').load('/melted/refresh #melted_status'); 
+
+   jQuery('#melted_status').load('/melted/refresh #melted_status');
 }
 jQuery(document).ready(function(){
-  refresh();
-  setInterval ("refresh()",2000);
+    refresh();
+      setInterval ("refresh()",2000);
 });
+*/
+
+// try with json, should be faster...
+ 
+  var refresh_content = function(data) {
+    jQuery('div.melted_status').html(data.data);
+  }
+ function refresh() {
+  var data;  
+    jQuery.ajax({
+        type: 'GET',
+        url: 'http://melted/melted/refresh',
+        data: data,
+        success: refresh_content,
+        dataType: 'json'
+      });
+  }
+ 
+  jQuery(document).ready(function() {
+    refresh();
+    setInterval  ("refresh()",2000);
+  });
+  
